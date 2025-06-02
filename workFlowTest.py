3d43af7d225f006ce4faa8d54980a7ad80fb5cf1
Anurag Srivastava <38259358+anuragbhumca07@users.noreply.github.com>
2025-06-02 02:01:51 -0400

    Update workFlowTest.py

--- workFlowTest.py
+++ workFlowTest.py
@@ -1,3 +1,13 @@
-print("Anurag1")
-sleep(60)
-print("Anurag2")
+name: Deploy to Cloud Run
+
+on:
+  push:
+    branches:
+      - main  # Change if your default branch is 'master'
+jobs:
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+      print("Anurag1")
+      sleep(60)
+      print("Anurag2")
